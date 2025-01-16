8d723ef7845ebce2283d3d64f6f2ec1db222eb8e
Puneet Vashisht <puneetvashisht@gmail.com>
2025-01-16 11:30:16 +0530

    rest apis

--- spring-boot-demo/spring-boot-demo/src/main/java/com/msci/spring_boot_demo/CourseController.java
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/msci/spring_boot_demo/CourseController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @RestController
@@ -24,17 +27,26 @@ public class CourseController {
         courses.add(new Course(5, "Geography", 9999));
     }
     
+
+    // get course by id
+    @RequestMapping(path="/courses/{id}", method=RequestMethod.GET)
+    public Course getCourseById(@PathVariable int id){
+        return courses.stream().filter(c -> c.getId() == id).findFirst().orElseThrow(() -> new CourseNotFoundException("Course not found"));
+    }
+
     @RequestMapping(path="/courses", method=RequestMethod.GET)
     public List<Course> getAllCourses(){
         return courses;
     }
 
+    @ResponseStatus(code = HttpStatus.CREATED)
     @RequestMapping(path="/courses", method=RequestMethod.POST)
     public void addCourse(@RequestBody Course course) {
         courses.add(course);
         // return "Course added successfully!";
     }
 
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @RequestMapping(path="/courses", method=RequestMethod.DELETE)
     public void deleteCourse(@RequestParam int id) {
         courses.removeIf(c -> c.getId() == id);
