5d21ba1e05990a1055c7c30740f3e1e12e920c75
Puneet Vashisht <puneetvashisht@gmail.com>
2025-01-16 12:41:12 +0530

    spring rest apis

--- spring-boot-data-demo/spring-boot-data-demo/src/main/java/com/msci/spring_boot_data_demo/Course.java
+++ spring-boot-data-demo/spring-boot-data-demo/src/main/java/com/msci/spring_boot_data_demo/Course.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+
+@Data
 @Entity
 public class Course {
     @Id
@@ -13,34 +16,34 @@ public class Course {
     String title;
     double price;
 
-    public Course(){
+    // public Course(){
         
-    }
+    // }
 
-    public Course(int id, String title, double price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-    }
+    // public Course(int id, String title, double price) {
+    //     this.id = id;
+    //     this.title = title;
+    //     this.price = price;
+    // }
     
-    public int getId() {
-        return id;
-    }   
+    // public int getId() {
+    //     return id;
+    // }   
     
-    public String getTitle() {
-        return title;
-    }
+    // public String getTitle() {
+    //     return title;
+    // }
     
-    public double getPrice() {
-        return price;
-    }
+    // public double getPrice() {
+    //     return price;
+    // }
     
-    public void setPrice(double newPrice) {
-        this.price = newPrice;
-    }
+    // public void setPrice(double newPrice) {
+    //     this.price = newPrice;
+    // }
     
-    @Override
-    public String toString() {
-        return "Course [id=" + id + ", title=" + title + ", price=" + price + "]";
-    }
+    // @Override
+    // public String toString() {
+    //     return "Course [id=" + id + ", title=" + title + ", price=" + price + "]";
+    // }
 }
